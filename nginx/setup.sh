003c2d73539eeaba3c36dcaf865b873e01640de6
Seb <sebastien.lutz@gmail.com>
2020-12-02 17:25:36 +0100

    Update setup.sh

--- nginx/setup.sh
+++ nginx/setup.sh
@@ -3,6 +3,8 @@
 apt -y install nginx
 
 sudo sed -i "s/www-data/$user $group/" /etc/nginx/nginx.conf
+sudo sed -i "/# gzip_vary/c\ \tgzip_vary on;" /etc/nginx/nginx.conf
+sudo sed -i "/# gzip_types/c\ \tgzip_types text\/plain text\/css application\/json application\/javascript text\/xml application\/xml application\/xml+rss text\/javascript;" /etc/nginx/nginx.conf
 
 echo "Starting nginx ..."
 sudo service nginx restart
