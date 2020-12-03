cb4a8bdb8c8053418e9ecaee07796cfad36ff581
Seb <sebastien.lutz@gmail.com>
2020-12-03 10:54:19 +0100

    Update setup.sh

--- mysql/setup.sh
+++ mysql/setup.sh
@@ -4,13 +4,13 @@
 sudo apt -y install mariadb-server mariadb-client
 #Create db and user
 sudo mysql -e "CREATE DATABASE IF NOT EXISTS  ${mysqlDB} /*\!40100 DEFAULT CHARACTER SET utf8 */;"
-mysqlUserSQLRow=$(sudo mysql -e "SELECT user,host FROM mysql.user WHERE user = '${user}' AND host = 'localhost';")
+mysqlUserSQLRow=$(sudo mysql -e "SELECT user,host FROM mysql.user WHERE user = '${mysqlUser}' AND host = 'localhost';")
 if [ -z "$mysqlUserSQLRow" ]; then
-    sudo mysql -e "CREATE USER ${user}@localhost IDENTIFIED BY '${mysqlPassword}';"
-    sudo mysql -e "GRANT ALL PRIVILEGES ON ${mysqlDB}.* TO '${user}'@'localhost';"
+    sudo mysql -e "CREATE USER ${mysqlUser}@localhost IDENTIFIED BY '${mysqlPassword}';"
+    sudo mysql -e "GRANT ALL PRIVILEGES ON ${mysqlDB}.* TO '${mysqlUser}'@'localhost';"
     sudo mysql -e "FLUSH PRIVILEGES;"
 else
-    sudo mysql -e "ALTER USER '${user}'@'localhost' IDENTIFIED BY '${mysqlPassword}'";
+    sudo mysql -e "ALTER USER '${mysqlUser}'@'localhost' IDENTIFIED BY '${mysqlPassword}'";
 fi
 
 echo "Starting mysql ..."
