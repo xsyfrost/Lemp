519b9d2961f788d6cb90037afe6c49ce182b0688
Seb <sebastien.lutz@gmail.com>
2020-12-03 08:34:33 +0100

    Update test.sh

--- test/test.sh
+++ test/test.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 
-phpModules=("curl" "gd2" "gettext" "intl" "mbstring" "exif" "mysqli" "pdo_mysql" "soap" "sqlite3")
-for t in ${phpModules[@]}; do
-  echo "$t"
-done
 
-phpVersion="7.4"
-echo ${phpVersion}
-
-echo "$(date +'%Y/%m/%d %H:%M:%S')FTP & Web user\nUsername:Hello\nPassword:Boy\n\nMysql & Phpmyadmin user\nUsername:Howareyou\nPassword:great" >> ./server_informations.txt
+source ./../default.conf
