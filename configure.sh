a583cfb258be3830a2598619d409b2b48d38cc14
Seb <sebastien.lutz@gmail.com>
2020-12-03 15:17:14 +0100

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -2,7 +2,7 @@
 
 read -p "Domain name (without www):" domain
 phpmyadminDomain=$(echo "phpmyadmin.$domain")
-phpmyadminIP=$(wget -qO - icanhazip.com)
+phpmyadminIP=$(who am i|awk '{ print $5}'| tr -d '()')
 
 sudo sed -i "/phpmyadminDomain=/c\phpmyadminDomain='$phpmyadminDomain'" ./default.conf
 sudo sed -i "/domain=/c\domain='$domain'" ./default.conf
