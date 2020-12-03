863f1b1eaef652f869560ae48ec05fa8916d17e2
Seb <sebastien.lutz@gmail.com>
2020-12-03 16:40:04 +0100

    Update setup.sh

--- mysql/setup.sh
+++ mysql/setup.sh
@@ -4,10 +4,12 @@
 sudo apt -y install mariadb-server mariadb-client
 #Create db and user
 sudo mysql -e "CREATE DATABASE IF NOT EXISTS  ${mysqlDB} /*\!40100 DEFAULT CHARACTER SET utf8 */;"
+sudo mysql -e "CREATE DATABASE IF NOT EXISTS phpmyadmin /*\!40100 DEFAULT CHARACTER SET utf8 */;"
 mysqlUserSQLRow=$(sudo mysql -e "SELECT user,host FROM mysql.user WHERE user = '${mysqlUser}' AND host = 'localhost';")
 if [ -z "$mysqlUserSQLRow" ]; then
     sudo mysql -e "CREATE USER ${mysqlUser}@localhost IDENTIFIED BY '${mysqlPassword}';"
     sudo mysql -e "GRANT ALL PRIVILEGES ON ${mysqlDB}.* TO '${mysqlUser}'@'localhost';"
+    sudo mysql -e "GRANT ALL PRIVILEGES ON phpmyadmin.* TO '${mysqlUser}'@'localhost';"
     sudo mysql -e "FLUSH PRIVILEGES;"
 else
     sudo mysql -e "ALTER USER '${mysqlUser}'@'localhost' IDENTIFIED BY '${mysqlPassword}'";
