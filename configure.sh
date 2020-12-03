f02b61e2ebee8a1899f371bb90091d50ed107cdd
Seb <sebastien.lutz@gmail.com>
2020-12-03 11:06:49 +0100

    Create configure.sh

---
+++ configure.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+read -p "Domain name (without www):" domain
+phpmyadminDomain=$(echo "www.$domain")
+phpmyadminIP=$(wget -qO - icanhazip.com)
+
+sudo sed -i "/phpmyadminDomain=/c\ phpmyadminDomain='$phpmyadminDomain'" ./default.conf
+sudo sed -i "/domain=/c\ domain='$domain'" ./default.conf
+sudo sed -i "/phpmyadminIP=/c\ phpmyadminIP='$phpmyadminIP'" ./default.conf
