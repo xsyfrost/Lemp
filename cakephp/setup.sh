97246641cb2e16b38fed4ea87d7c799bb1eaa041
Seb <sebastien.lutz@gmail.com>
2020-12-03 12:26:15 +0100

    Update setup.sh

--- cakephp/setup.sh
+++ cakephp/setup.sh
@@ -5,13 +5,13 @@ if [ -d "${cakephpDir}" ]; then
     mv ${cakephpDir} $(echo "$cakephpDir_$random")
 fi
 mkdir -p ${cakephpDir}
-sudo php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-sudo php -r "if (hash_file('sha384', 'composer-setup.php') === '756890a4488ce9024fc62c56153228907f1545c228516cbf63f885e036d37e9a59d27d63f46af1d4d07ee0f76181c7d3') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
-sudo php composer-setup.php
-sudo php -r "unlink('composer-setup.php');"
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php -r "if (hash_file('sha384', 'composer-setup.php') === '756890a4488ce9024fc62c56153228907f1545c228516cbf63f885e036d37e9a59d27d63f46af1d4d07ee0f76181c7d3') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
 sudo mv composer.phar /usr/local/bin/composer
 sudo composer create-project --no-interaction --prefer-dist cakephp/app:~${cakephpVersion} ${cakephpDir}
 if [ -n "$cakephpModules" ]; then
     currentPath=$(pwd)
-    cd ${cakephpDir} && sudo composer --no-interaction require ${cakephpModules} && cd ${currentPath}
+    cd ${cakephpDir} && sudo composer --no-interaction require ${cakephpModules} && cd ${currentPath} || cd ${currentPath}
 fi
